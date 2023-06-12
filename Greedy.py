dbf438b226d624a506806f99480d0a2e53657a4f
mirkowoo <marcosfranichevicmuixi@gmail.com>
2023-06-11 21:10:05 -0400

    problem after problem
    I managed to build some sort of esqueleton for the program, changed the data in the sets (there was some missing info), but still having trouble deleting the "bestSet" from "notUsed"

--- Greedy.py
+++ Greedy.py
@@ -1,88 +1,59 @@
 import random
 import math
-
-class Comuna:
-    def __init__(self,id,costo,vecinos,uso):
-        self.id = id
-        self.costo = costo
-        self.vecinos = vecinos
-        self.uso = uso   
-
-def cargarDatos():
-    comunas = [Comuna(2,1,[4,5],0)]
-    comunas.append(Comuna(3,1.5,[5,27,29],0))
-    comunas.append(Comuna(4,1.2,[2,25,26,28],0))
-    comunas.append(Comuna(5,2,[2,3,28,29],0))
-    comunas.append(Comuna(6,3,[7,9,15,24,27],0))
-    comunas.append(Comuna(7,2,[6,8,9,10,15],0))
-    comunas.append(Comuna(8,1,[7,10],0))
-    comunas.append(Comuna(9,1,[6,7],0))
-    comunas.append(Comuna(10,3,[7,8,15,33],0))
-    comunas.append(Comuna(11,4,[12,16,17,24,25],0))
-    comunas.append(Comuna(12,3,[11,13,15],0))
-    comunas.append(Comuna(13,3,[12,15,17,33,35],0))
-    comunas.append(Comuna(14,2,[16,17,30,31,37],0))
-    comunas.append(Comuna(15,2.5,[6,7,10,12,13],0))
-    comunas.append(Comuna(16,1.5,[11,14,17,30],0))
-    comunas.append(Comuna(17,2,[11,13,14,16,33,35],0))
-    comunas.append(Comuna(18,2,[20,34],0))
-    comunas.append(Comuna(19,3,[21,22,34],0))
-    comunas.append(Comuna(20,2,[18,21,34],0))
-    comunas.append(Comuna(21,2,[19,20,34],0))
-    comunas.append(Comuna(22,3,[19,23],0))
-    comunas.append(Comuna(23,2,[22],0))
-    comunas.append(Comuna(24,3,[6,11,15,25,26,27,28],0))
-    comunas.append(Comuna(25,3,[11,24,26],0))
-    comunas.append(Comuna(26,1,[4,24,25,28],0))
-    comunas.append(Comuna(27,2.5,[3,6,28,29],0))
-    comunas.append(Comuna(28,2,[4,5,24,26,27,29],0))
-    comunas.append(Comuna(29,3.5,[3,5,27,28],0))
-    comunas.append(Comuna(30,2,[14,16,31,34,36,37],0))
-    comunas.append(Comuna(31,1.5,[14,35,38],0))
-    comunas.append(Comuna(33,2,[10,13,15,17,35],0))
-    comunas.append(Comuna(34,3,[18,19,20,21,30,36],0))
-    comunas.append(Comuna(35,3.5,[17,31,33],0))
-    comunas.append(Comuna(36,2,[30,34,38],0))
-    comunas.append(Comuna(37,2.5,[14,30],0))
-    comunas.append(Comuna(38,1.5,[30,31,36],0))
-    return comunas
+import time
 
 def algorythm(universe,subSets,costs):
 
     #set used to save the selected subsets
-    selected =set()
+    selected = []
     #set that represent the elements that are not used yet
-    notUsed = subSets
-
+    notUsed = frozenset(universe)
     while notUsed:
-        bestsubset = None
-        bestRatio = 0
-        for Set in subSets:
-            
-            
-        
+        bestSubset = None
+        bestRatio = -0.1
+        print(len(subSets))
+        for i in subSets:
+            print(i)
+            cost = costs[frozenset(i)]
+            costRatio = cost / len(subSets)
+            if not frozenset(i) in notUsed:
+                if costRatio > bestRatio:
+                    bestSubset = i
+                    bestRatio = costRatio
+        selected.append(bestSubset)
+        #print(bestSubset)
+        print()
+        time.sleep(1)
+        notUsed.remove(frozenset(bestSubset))
+    return selected 
 
 #universe: 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,33,34,35,36,37,38
 #subsets: [4,5],[5,27,29],[2,25,26,28],[2,3,28,29],[7,9,15,24,27],[6,8,9,10,15],[7,10],[6,7],[7,8,15,33],[12,16,17,24,25],[11,13,15],[12,15,17,33,35],[16,17,30,31,37],[6,7,10,12,13],[11,14,17,30],[11,13,14,16,33,35],[20,34],[21,22,34],[18,21,34],[19,20,34],[19,23],[22],[6,11,15,25,26,27,28],[11,24,26],[4,24,25,28],[3,6,28,29],[4,5,24,26,27,29],[3,5,27,28],[14,16,31,34,36,37],[14,35,38],[10,13,15,17,35],[18,19,20,21,30,36],[17,31,33],[30,34,38],[14,30],[30,31,36]
 #costs: 1,1.5,1.2,2,3,2,1,1,3,4,3,3,2,2.5,1.5,2,2,3,2,2,3,2,3,3,1,2.5,2,3.5,2,1.5,2,3,3.5,2,2.5,1.5
 
-
 #id de cada comuna
-universe = set(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,33,34,35,36,37,38)
+universe = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,33,34,35,36,37,38]
 #subsets del universo
-subSets = set([4,5],[5,27,29],[2,25,26,28],[2,3,28,29],[7,9,15,24,27],[6,8,9,10,15],[7,10],[6,7],[7,8,15,33],[12,16,17,24,25],[11,13,15],[12,15,17,33,35],[16,17,30,31,37],[6,7,10,12,13],[11,14,17,30],[11,13,14,16,33,35],[20,34],[21,22,34],[18,21,34],[19,20,34],[19,23],[22],[6,11,15,25,26,27,28],[11,24,26],[4,24,25,28],[3,6,28,29],[4,5,24,26,27,29],[3,5,27,28],[14,16,31,34,36,37],[14,35,38],[10,13,15,17,35],[18,19,20,21,30,36],[17,31,33],[30,34,38],[14,30],[30,31,36])
-#costos¿
-costs = set(1,1.5,1.2,2,3,2,1,1,3,4,3,3,2,2.5,1.5,2,2,3,2,2,3,2,3,3,1,2.5,2,3.5,2,1.5,2,3,3.5,2,2.5,1.5)
-
+subSets = [{2,4,5},{3,5,27,29},{4,2,25,26,28},{5,2,3,28,29},{6,7,9,15,24,27},{7,6,8,9,10,15},{8,7,10},{9,6,7},{10,7,8,15,33},
+           {11,12,15,16,17,24,25},{12,11,13,15},{13,12,15,17,33},{14,16,17,30,31,37},{15,6,7,10,11,12,13,24,33},{16,11,14,17,30},{17,11,13,14,16,33,35},
+           {18,20,34},{19,21,22,34},{20,18,21,34},{21,19,20,34},{22,19,23},{23,22},{24,6,11,15,25,26,28},{25,4,11,24,26},{26,4,24,25,28},{27,3,6,28,29},{28,4,5,24,26,27,29},{29,3,5,27,28},{30,14,16,34,36,37,38},{31,14,35,38},
+           {33,10,13,15,17,35},{34,18,19,20,21,30,36},{35,17,31,33},{36,30,34,38},{37,14,30},{38,30,31,36}]
+#[{4,5},{5,27,29},{2,25,26,28},{2,3,28,29},{7,9,15,24,27},{6,8,9,10,15},{7,10},{6,7},{7,8,15,33},{12,16,17,24,25},{11,13,15},{12,15,17,33,35},{16,17,30,31,37},{6,7,10,12,13},{11,14,17,30},{11,13,14,16,33,35},{20,34},{21,22,34},{18,21,34},{19,20,34},{19,23},{22},{6,11,15,25,26,27,28},{11,24,26},{4,24,25,28},{3,6,28,29},{4,5,24,26,27,29},{3,5,27,28},{14,16,31,34,36,37},{14,35,38},{10,13,15,17,35},{18,19,20,21,30,36},{17,31,33},{30,34,38},{14,30},{30,31,36}]
+#costos
+#costs = [1,1.5,1.2,2,3,2,1,1,3,4,3,3,2,2.5,1.5,2,2,3,2,2,3,2,3,3,1,2.5,2,3.5,2,1.5,2,3,3.5,2,2.5,1.5]
+#dictionary of costs
+costs = {frozenset({2,4,5}): 1,frozenset({3,5,27,29}):1.5,frozenset({4,2,25,26,28}): 1.2,frozenset({5,2,3,28,29}):2,frozenset({6,7,9,15,24,27}):3,frozenset({7,6,8,9,10,15}):2,frozenset({8,7,10}):1,frozenset({9,6,7}):1,frozenset({10,7,8,15,33}):3,
+         frozenset({11,12,15,16,17,24,25}):4,frozenset({12,11,13,15}):3,frozenset({13,12,15,17,33}):3,frozenset({14,16,17,30,31,37}):2,frozenset({15,6,7,10,11,12,13,24,33}):2.5,frozenset({16,11,14,17,30}):1.5,frozenset({17,11,13,14,16,33,35}):2,
+         frozenset({18,20,34}):2,frozenset({19,21,22,34}):3,frozenset({20,18,21,34}):2,frozenset({21,19,20,34}):2,frozenset({22,19,23}):3,frozenset({23,22}):2,frozenset({24,6,11,15,25,26,28}):3,frozenset({25,4,11,24,26}):3,
+         frozenset({26,4,24,25,28}):1,frozenset({27,3,6,28,29}):2.5,frozenset({28,4,5,24,26,27,29}):2,frozenset({29,3,5,27,28}):3.5,frozenset({30,14,16,34,36,37,38}):2,frozenset({31,14,35,38}):1.5,
+         frozenset({33,10,13,15,17,35}):2,frozenset({34,18,19,20,21,30,36}):3,frozenset({35,17,31,33}):3.5,frozenset({36,30,34,38}):2,frozenset({37,14,30}):2.5,frozenset({38,30,31,36}):1.5}
+'''
+{frozenset({4,5}): 1,frozenset({5,27,29}):1.5,frozenset({2,25,26,28}): 1.2,frozenset({2,3,28,29}):2,frozenset({7,9,15,24,27}):3,frozenset({6,8,9,10,15}):2,frozenset({7,10}):1,frozenset({6,7}):1,frozenset({7,8,15,33}):3,frozenset({12,16,17,24,25}):4,frozenset({11,13,15}):3,frozenset({12,15,17,33,35}):3
+ ,frozenset({16,17,30,31,37}):2,frozenset({6,7,10,12,13}):2.5,frozenset({11,14,17,30}):1.5,frozenset({11,13,14,16,33,35}):2,frozenset({20,34}):2,frozenset({21,22,34}):3
+ ,frozenset({18,21,34}):2,frozenset({19,20,34}):2,frozenset({19,23}):3,frozenset({22}):2,frozenset({6,11,15,25,26,27,28}):3,frozenset({11,24,26}):3,frozenset({4,24,25,28}):1,frozenset({3,6,28,29}):2.5,frozenset({4,5,24,26,27,29}):2,frozenset({3,5,27,28}):3.5,frozenset({14,16,31,34,36,37}):2
+ ,frozenset({14,35,38}):1.5,frozenset({10,13,15,17,35}):2,frozenset({18,19,20,21,30,36}):3,frozenset({17,31,33}):3.5,frozenset({30,34,38}):2,frozenset({14,30}):2.5,frozenset({30,31,36}):1.5}
+'''
 print(algorythm(universe,subSets,costs))
-
-    
-    
-
-    
-    
-    
-
 
 #comunas = cargarDatos()
 #print(len(universe))
---
+++ test.py
@@ -0,0 +1,8 @@
+A = {10, 20, 30, 40, 80}
+B = {100, 30, 80, 40, 60}
+ 
+# Modifies A and returns None
+A.difference_update(B)
+ 
+# Prints the modified set
+print (A)
